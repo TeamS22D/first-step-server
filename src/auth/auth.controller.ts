dfcf3b0cb8217658548d7f5514501abdadac78a2
hyuntroll <83080414+hyuntroll@users.noreply.github.com>
2025-10-01 18:03:49 +0900

    refactor: move signin logic from AuthController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -10,7 +10,6 @@ import {
 } from '@nestjs/common';
 
 import { JwtService } from '@nestjs/jwt';
-import * as bcrypt from 'bcrypt';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthDTO } from './dto/auth-dto';
 import { SocialUserDto } from './dto/social-user.dto';
@@ -21,29 +20,13 @@ import type { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly userService: UserService, private readonly jwtService: JwtService, private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService
+  ) {}
 
   @Post('/signin')
   async signin(@Body() authDTO: AuthDTO.SignIn) {
-    const { email, password } = authDTO;
-
-    const user = await this.userService.findByEmail(email);
-    if (!user) {
-      throw new UnauthorizedException({message: "이메일이나 비밀번호를 확인해주십시오"})
-    }
-    const samePassword = bcrypt.compareSync(password, user.password)
-    if (!samePassword) {
-      throw new UnauthorizedException({message: "이메일이나 비밀번호를 확인해주십시오"})
-    }
-
-    const payload = {
-      id: user.id,
-    }
-
-
-    const accessToken = this.jwtService.sign(payload);
-
-    return {'accessToken' : accessToken};
+      return this.authService.signin(authDTO)
   }
 
   @Get('google')
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,44 @@
+import {
+  UnauthorizedException
+} from '@nestjs/common'
 
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { SocialUserDto } from './dto/social-user.dto';
+import { AuthDTO } from './dto/auth-dto';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
+
+
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly jwtService: JwtService) {}
+  constructor(
+    private readonly jwtService: JwtService, 
+    private readonly userService: UserService
+  ) {}
+
+  async signin(autoDTO: AuthDTO.SignIn) {
+    const { email, password } = autoDTO
+
+    const user = await this.userService.findByEmail(email);
+        if (!user) {
+          throw new UnauthorizedException({message: "이메일이나 비밀번호를 확인해주십시오"})
+        }
+        const samePassword = bcrypt.compareSync(password, user.password)
+        if (!samePassword) {
+          throw new UnauthorizedException({message: "이메일이나 비밀번호를 확인해주십시오"})
+        }
+    
+        const payload = {
+          id: user.id,
+        }
+
+        const accessToken = this.jwtService.sign(payload);
+
+        return {'accessToken': accessToken}
+
+  }
 
   async socialLogin(userDto: SocialUserDto) {
     console.log('소셜 로그인 유저 정보:', userDto);
