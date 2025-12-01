faca8cb7f4b093fe1bae1c76e83a6c1019a0b052
hyuntroll <83080414+hyuntroll@users.noreply.github.com>
2025-12-02 08:54:09 +0900

    refactor: missionTheme을 enum으로 변경

--- src/mission/entities/mission.entity.ts
+++ src/mission/entities/mission.entity.ts
@@ -12,6 +12,7 @@ import { Rubric } from '../../rubric/entities/rubric.entity';
 import { UserMission } from '../../user-mission/entities/user-mission.entity';
 import { forwardRef } from '@nestjs/common';
 import { Provider } from '../../auth/dto/social-user.dto';
+import { MissionTheme } from '../types/missoin-theme.enum';
 
 @Entity({ name: 'mission' })
 export class Mission {
@@ -31,8 +32,12 @@ export class Mission {
   @Column({ nullable: false })
   missionName: string;
 
-  @Column()
-  missionTheme: string;
+  @Column({
+    type: 'enum',
+    enum: MissionTheme,
+    default: 'document',
+  })
+  missionTheme: MissionTheme;
 
   @Column({ type: 'text', nullable: false })
   body: string;
